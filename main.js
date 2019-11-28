f9c9667b701ade0c0b3c0d0581d200103af45303
Larissa <larissa.silva@cubos.io>
2019-11-28 10:57:31 -0200

    feat: add font family && adjustment js

--- main.js
+++ main.js
@@ -1,60 +1,74 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  var cronometro = function() {
+    var monsths = [
+      "",
+      "Jan",
+      "Feb",
+      "Mar",
+      "May",
+      "Apr",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
 
-	var cronometro = function(){
+    var dataFinal = "10/12/2019";
+    var horaFinal = "00:00:00";
 
-		var monsths = ["", "Jan", "Feb", "Mar", "May", "Apr", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var dataSplited = dataFinal.split("/");
 
-		var dataFinal = "21/07/2018";
-		var horaFinal = "00:00:00";
+    var dia = parseInt(dataSplited[0], 10);
+    var mes = monsths[parseInt(dataSplited[1], 10)];
+    var ano = parseInt(dataSplited[2], 10);
 
-		var dataSplited = dataFinal.split("/");
+    var dataTo = mes + " " + dia + ", " + ano + " " + horaFinal;
 
-		var dia = parseInt(dataSplited[0], 10);
-		var mes = monsths[parseInt(dataSplited[1], 10)];
-		var ano = parseInt(dataSplited[2], 10);
+    var dataFim = new Date(dataTo).getTime();
+    var agora = new Date().getTime();
 
-		var dataTo = mes+" "+dia+", "+ano+" "+horaFinal;
+    var distancia = dataFim - agora;
 
-		var dataFim = new Date(dataTo).getTime();
-		var agora = new Date().getTime();
+    var segundo = 1000;
+    var minuto = segundo * 60;
+    var hora = minuto * 60;
+    var dia = hora * 24;
 
-		var distancia = dataFim - agora;
+    var dias = Math.floor(distancia / dia);
+    var horas = Math.floor((distancia % dia) / hora);
+    var minutos = Math.floor((distancia % hora) / minuto);
+    var segundos = Math.floor((distancia % minuto) / segundo);
 
-		var segundo = 1000;
-		var minuto =  segundo * 60;
-		var hora =  minuto * 60;
-		var dia = hora * 24;
+    if (dias > 1) {
+      var diasConcate = dias + " dias ";
+    } else {
+      var diasConcate = dias + " dia ";
+    }
 
-		var dias = Math.floor(distancia / (dia));
-		var horas = Math.floor((distancia % dia) / hora);
-		var minutos = Math.floor((distancia  % hora) / minuto);
-		var segundos = Math.floor((distancia % minuto) / segundo); 
+    var horasConcate = horas + "h ";
+    var minutosConcate = minutos + "m ";
+    var segundosConcate = segundos + "s ";
+    var totalConcate =
+      diasConcate + horasConcate + minutosConcate + segundosConcate;
 
-		if(dias > 1){
-			var diasConcate = dias+" dias ";
-		} else {
-			var diasConcate = dias+" dia ";
-		}
+    if (distancia < 0) {
+      $("#contador")
+        .html("Expirou o tempo!")
+        .css("color", "red");
+      $("#btnSlider")
+        .attr("disabled", "disabled")
+        .children(".btnInnerText")
+        .text("Encerrado")
+        .css("text-transform", "uppercase");
+    } else {
+      $("#contador").html(totalConcate);
+    }
 
-		var horasConcate = horas+"h ";
-		var minutosConcate = minutos+"m ";
-		var segundosConcate = segundos+"s ";
-		var totalConcate = diasConcate + horasConcate + minutosConcate + segundosConcate;
+    setTimeout(cronometro, 1000);
+  };
 
-
-		if (distancia < 0) {
-			$("#contador").html('Expirou o tempo!').css('color','red');
-			$("#btnSlider").attr("disabled", "disabled").children(".btnInnerText").text("Encerrado").css("text-transform","uppercase");
-		} else {
-			$("#contador").html(totalConcate);
-		}
-
-		setTimeout(cronometro, 1000);
-
-	}
-
-	cronometro();
-
-
+  cronometro();
 });
-
